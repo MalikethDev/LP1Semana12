dda75d247da2b2e0ac228b7955c8037244f4e55e
Rodrigo <rodrigogomes091997@gmail.com>
2025-05-22 17:06:15 +0100

    Add BeforeUtils class and test it with class Program

---
+++ TheyCameBefore/BeforeUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TheyCameBefore
+{
+    public static class BeforeUtils
+    {
+        public static IEnumerable<T> GetTheOnesBefore<T>(IEnumerable<T> items, T threshold)
+            where T : struct, IComparable<T>
+        {
+            foreach (T item in items)
+            {
+                if (item.CompareTo(threshold) < 0)
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
